093163aa571c0bdc6423577f3e9db82f984774a6
cristian <cbarrerar@unal.edu.co>
2025-04-30 20:58:22 -0500

    Add: solution to Add Two Numbers problem

--- src/Problems/AddTwoNumbers.java
+++ src/Problems/AddTwoNumbers.java
@@ -1,5 +1,6 @@
 package Problems;
 
+import java.util.Arrays;
 /*
  *Problem
  https://leetcode.com/problems/add-two-numbers/description/
@@ -7,27 +8,40 @@ package Problems;
 
 public class AddTwoNumbers {
     
-    public static void solution(){
-        
-        int algo[] = {0,1,2,3,4,5,6};
-        int algo2[] = new int[algo.length];
-    
-        int counter = 0;
-        for(int i = algo.length; i > 0 ; i--){
-    
-            algo2[counter++] = algo[i-1];
-        }
-    
-        for(int i = 0; i < algo2.length; i++){
-            System.out.print(algo2[i] + " ");
-        }
-        System.out.println();
-        for(int i = 0; i < algo2.length; i++){
-            System.out.print(algo[i] + " ");
-        }
-        System.out.println("");
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
     }
-    
 
-    
+    public ListNode solution(ListNode l1, ListNode l2) {
+
+        ListNode solution = new ListNode(0);
+        ListNode current = solution;
+        int carry = 0;
+
+        while (l1 != null || l2!= null || carry != 0) {
+            int sum = carry;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            carry = sum / 10;                
+            current.next = new ListNode(sum % 10);
+            current = current.next;   
+
+        }
+
+        return solution.next;
+    }
 }
+   
+
