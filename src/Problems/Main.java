94c25f0ebc18e9627d62d5fbd2a3cc6fc579ea23
cristian <cbarrerar@unal.edu.co>
2025-04-30 21:14:03 -0500

    Fixed main class and other files

--- src/Problems/Main.java
+++ src/Problems/Main.java
@@ -6,11 +6,6 @@ public class Main {
     
         System.out.println("Main Class");
         
-        int nums1[] = {2,5,6,0,0,0};
-        int nums2[] = {1,2,3};
-        
-        MergeSortedArray.merge(nums1, 3, nums2, 3);
-        
-
+        FindNumbersWithEvenNimberOfDigits.solution();
     }
 }
